9ee82844165109433ff61f66f6bb47219a9bbbf7
ulgerdc <56354881+ulgerdc@users.noreply.github.com>
2023-12-07 13:01:31 +0000

    changes

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,11 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "What are the critical database tables?",
+        value: "What are the critical database tables?"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "How To Store Enums in Database?", value: "How To Store Enums in DB?" },
+    { text: "What are the database rules?", value: "What are the database rules?" }
 ];
 
 interface Props {
