579ff0b93fd652df5d8ca08d44a6a6a5610b7c31
ulgerdc <56354881+ulgerdc@users.noreply.github.com>
2023-12-07 13:22:56 +0000

    changes

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet, NavLink, Link } from "react-router-dom";
 
-import github from "../../assets/github.svg";
+import github from "../../assets/fimpleicon.svg";
 
 import styles from "./Layout.module.css";
 
--- app/frontend/src/pages/oneshot/OneShot.tsx
+++ app/frontend/src/pages/oneshot/OneShot.tsx
@@ -146,13 +146,9 @@ export function Component(): JSX.Element {
         <div className={styles.oneshotContainer}>
             <div className={styles.oneshotTopSection}>
                 <SettingsButton className={styles.settingsButton} onClick={() => setIsConfigPanelOpen(!isConfigPanelOpen)} />
-                <h1 className={styles.oneshotTitle}>Ask your data</h1>
+                <h1 className={styles.oneshotTitle}>Chat with Fimple's SDK</h1>
                 <div className={styles.oneshotQuestionInput}>
-                    <QuestionInput
-                        placeholder="Example: Does my plan cover annual eye exams?"
-                        disabled={isLoading}
-                        onSend={question => makeApiRequest(question)}
-                    />
+                    <QuestionInput placeholder="Example: How To Check Concurrent Update?" disabled={isLoading} onSend={question => makeApiRequest(question)} />
                 </div>
             </div>
             <div className={styles.oneshotBottomSection}>
